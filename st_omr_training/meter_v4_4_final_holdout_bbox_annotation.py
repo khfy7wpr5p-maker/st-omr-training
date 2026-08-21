15c28aa2fc80180b364c6caa2b9627fbd5afaf19
khfy7wpr5p-maker <onderozudogru@gmail.com>
2026-08-21 22:01:18 +0300

    Meter V4-4: expose annotation facade

---
+++ st_omr_training/meter_v4_4_final_holdout_bbox_annotation.py
@@ -0,0 +1,5 @@
+"""Public facade for Meter V4-4 final holdout bbox annotation."""
+
+from st_omr_training.meter_v4_4_bbox_contract import *  # noqa: F401,F403
+from st_omr_training.meter_v4_4_bbox_state import *  # noqa: F401,F403
+from st_omr_training.meter_v4_4_bbox_qa import *  # noqa: F401,F403
